519d6a8470b18dd55d15dc45571c66e95d7f32d8
Asher Ma <Asher Ma>
2016-10-06 22:53:43 -0700

    HW2 implemented in 2 versions_with both javascript and jquery

---
+++ HW2/myJquery.js
@@ -0,0 +1,53 @@
+function cal(){
+    if(isNaN($("#purchase").val()) || isNaN($("#down").val()) || $("#purchase").val()=="" || $("#down").val()=="" || parseFloat(($("#purchase").val()) ) < parseFloat(($("#down").val())) ) {
+        alert("input number only & purchase price has to be greater than down payment.");
+    }
+    else{
+        var dn = parseFloat($("#down").val());
+        var pur = parseFloat($("#purchase").val());
+        var prin = pur - dn;
+        var n = parseFloat($("input[type='radio']:checked").val());
+        alert("You selected "+n+" year fixed");
+        var r=checkRate(n);
+        alert("Your rate is "+r);
+        var payment = prin*(r/12)*( Math.pow( (1+r/12),n*12) )/((Math.pow((1+r/12),n*12))-1);
+        
+        var tableString = `
+            <table class="center table table-border table-hover">
+                <tr>
+                    <th scope="col"></td>
+                    <th class="center" scope="col">`+n+` yr fixed</td>
+                    
+                </tr>
+                <tr>
+                    <th scope="row">Payment</th>
+                    <td class="center">`+payment.toFixed(2)+`</td>
+                </tr>  
+                <tr>
+                    <th scope="row">Interest Rate</th>
+                    <td class="center">`+(r*100).toFixed(2)+`%</td>
+                </tr> 
+            </table>`
+        
+        $("#blank").append(tableString);
+    }
+}
+                            
+function checkRate(field){
+    var r;
+    if(field==15){
+                r=0.0275;
+                }
+        if(field==30){
+                r=0.0350;
+            }
+    return r;
+    
+}
+$("#sub").click(function(){
+                          $("#head").html("Your Payment below").css("background-color","grey");  
+                          $("div#blank").empty();
+                          cal();   
+                          $("<a id='set' href='https://www.bankofamerica.com/mortgage/home-mortgage/' target='_blank'>need more info? click me.</a>").appendTo("div#blank");
+                          });
+
