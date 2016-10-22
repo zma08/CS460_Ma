a768223bf3e893b259142de472c43eebedb397da
Asher Ma <Asher Ma>
2016-10-21 23:18:28 -0700

    task2 is done

--- HW4/AshLoan/AshLoan/Controllers/HomeController.cs
+++ HW4/AshLoan/AshLoan/Controllers/HomeController.cs
@@ -20,14 +20,13 @@ namespace AshLoan.Controllers
         {
 
             int n;
-            Debug.WriteLine(Request.HttpMethod);
-            string name = Request.QueryString["fName"];
+            Debug.WriteLine(Request.HttpMethod);//http method
+            string name = Request.QueryString["fName"];//get user input from querystring from the url
             string age = Request.QueryString["age"];
-            Debug.WriteLine(Request.Form.Count);//there is not form data at a Http get 
+           // Debug.WriteLine(Request.Form.Count);//there is not form data at a Http get 
             Debug.WriteLine("name: {0}, age: {1}",name, age);
-            Debug.WriteLine("name: {0}, age: {1}", Request.QueryString["fName"], Request.QueryString["age"]);
             
-            if (String.IsNullOrWhiteSpace(name) || !Int32.TryParse(age, out n))
+            if (String.IsNullOrWhiteSpace(name) || !Int32.TryParse(age, out n))//if the input text is empty or white space or the age is not a number or empty return a message to user
             {
                 ViewBag.message = "please input your name and your age should be a integer";
             }
@@ -35,6 +34,7 @@ namespace AshLoan.Controllers
             {
                 int age0 = Convert.ToInt32(age);
                 Debug.WriteLine(n==age0);
+                //pre-build a message, when the user info come in and it will return a message to user
                 if (age0< 10)
                 {
                     ViewBag.message = String.Format("Hi {0},you are a baby.", name);
@@ -45,7 +45,7 @@ namespace AshLoan.Controllers
                 }
                 if (age0 < 60 && age0 >= 19)
                 {
-                    ViewBag.message = String.Format("Hi {0},you are a adult.", name);
+                    ViewBag.message = String.Format("Hi {0},you are an adult.", name);
                 }
                 if (age0 > 60)
                 {
@@ -57,13 +57,38 @@ namespace AshLoan.Controllers
             return View();
         }
 
+        [HttpGet]//this is a http get method
         public ActionResult Task2()
         {
-            ViewBag.Message = "Your contact page.";
-
+            
+            Debug.WriteLine("we are in Task2 "+Request.HttpMethod);//indentify that this is a http get method
             return View();
         }
 
+        [HttpPost]//this is http post method
+        public ActionResult Task2(FormCollection f)
+        {
+            NameValueCollection a = Request.Form;//
+            Debug.WriteLine("We are in Task2 "+Request.HttpMethod);
+            Debug.WriteLine(a.Count);//in post method form data will be submit
+            if (String.IsNullOrWhiteSpace(f["Name"]) || ( (!f["Employed"].Equals("yes") ) && (!f["Employed"].Equals("no")))
+)            {
+                ViewBag.Message = "please enter your name and type yes or no ony for employed status";
+            }
+            else
+            {
+                if (f["Employed"].Equals("yes"))
+                {
+                    ViewBag.Message = f["Name"]+", working hard is good for you (;";
+                }
+                else
+                {
+                    ViewBag.Message = f["Name"]+", your should go get a job or go to school (;";
+                }
+            }
+
+            return View();
+        }
         public ActionResult Loan()
         {
             return View();
