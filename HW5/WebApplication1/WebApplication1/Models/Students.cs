fdc21426f3a210e0cf3a79911916f8dc150abd0c
Asher Ma <Asher Ma>
2016-10-27 11:03:42 -0700

    hw5 start at master and checking out to branch now

---
+++ HW5/WebApplication1/WebApplication1/Models/Students.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Students
+    {
+        [Required]
+        public int VNumber { get; set; }
+        [Required]
+        [Display (Name = "First Nmae:")]
+        public string firstN { get; set; }
+        [Required]
+        [Display (Name = "Last Name")]
+        public string LastN { get; set; }
+        [Required]
+        [Display(Name = "Date Name")]
+        public string Date { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Major")]
+        public string Major { get; set; }
+        [Required]
+        [Display(Name = "Minor")]
+        public string Minor { get; set; }
+        [Required]
+        [Display(Name = "Advisor")]
+        public string Advisor { get; set; }
+    }
+}
