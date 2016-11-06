1708bc268665cd8242c6935b2597cb6aaaf16cf9
Asher Ma <Asher Ma>
2016-11-06 15:07:53 -0800

    MVC pattern interact with relational database include lazy loading, generate product with looping thru the category and subcategory with id passed by actionlink helper method to access data from database including set up image path to retrive image from database as well

---
+++ HW6/Adventure14/Adventure14/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;  
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Adventure14.Models;
+
+namespace Adventure14.Controllers
+{
+    public class ProductController : Controller
+    {
+        AdventureContext db = new AdventureContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            
+            return View(db.ProductCategories.ToList());
+        }
+        public ActionResult ProductItem(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            else
+            {
+                return View(db.Products.Where(pk => pk.ProductSubcategoryID == id).ToList());
+            }
+           
+        }
+        [HttpGet]
+        public ActionResult Review(int id)
+        {
+            
+                ProductReview productReview = new ProductReview();
+                productReview.ProductID = id;
+                return View(productReview);
+            
+        }
+           
+        [HttpPost]
+        public ActionResult Review(ProductReview productReview)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductReviews.Add(productReview);
+                db.SaveChanges();
+               // Debug.WriteLine(productReview.ReviewerName);
+                return View("Thanks",productReview);
+            }
+            else
+            {
+                return View(productReview);
+            }
+           
+
+        }
+        public ActionResult ListAllReviews()
+        {
+            return View(db.ProductReviews.ToList());
+        }
+
+       /* public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductCategories.Add(productCategory);
+                db.SaveChanges();
+                return RedirectToAction("Show");
+            }
+            else
+            {
+                return View(productCategory);
+            }
+        }
+        public ActionResult Show()
+        {
+           
+            return View(db.ProductCategories.ToList());
+        }*/
+
+    }
+}
---
+++ HW6/Adventure14/Adventure14/Models/Prvm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adventure14.Models
+{
+    public class Prvm
+    {
+        public Product Product { get; set; }
+        public ProductReview PReview { get; set; }
+        public int PId { get; set; }
+    }
+}
